10e562684cd3e042adec70dd7928d979587fbd1e
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2024-05-08 22:47:18 +0300

    first model created

---
+++ rentACar/Domain/Models/Brand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Brand
+    {
+        public int ID { get; set; }
+
+        public string NAME { get; set; }
+
+        public bool IS_DELETED { get; set; }
+    }
+}
